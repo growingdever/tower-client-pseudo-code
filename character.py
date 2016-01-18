3e27a4d2f8382325036112a2a56e3c86fa3b7a27
growingdever <growingdever@gmail.com>
2016-01-18 11:02:30 +0900

    no message

--- character.py
+++ character.py
@@ -1,5 +1,5 @@
 class Character():
-	ability
+	ability, dirtyAbility
 	currState
 
 	def OnDead():
@@ -12,12 +12,35 @@ class Character():
 		pass
 
 	def OnUpdate():
-		dirtyAbility = ability
-		for buf in bufList:
-			buf.apply(dirtyAbility)
-
 		currState.OnUpdate(this)
 
+		for buff in buffList:
+			buff.OnUpdate(this)
+
+		pass
+
+	def OnStatusChanged():
+		dirtyAbility = ability
+		
+		for buff in buffList:
+			if buff.type == 'UPGRADE':
+				buff.apply(dirtyAbility)
+
+		for equipment in equipments:
+			equipment.apply(dirtyAbility)
+
+	def OnSendHitResult(hitResult):
+		for buff in buffList:
+			if buff.type == 'ADDITIONAL_HIT_RESULT':
+				buff.OnSendHitResult(hitResult)
+		pass
+
+	def OnRecvHitResult(hitResult):
+		dirtyAbility.apply(hitResult)
+
+		for buff in buffList:
+			if buff.type == 'REACT':
+				buff.OnRecvHitResult(hitResult)
 		pass
 
 	def OnStepChanged():
